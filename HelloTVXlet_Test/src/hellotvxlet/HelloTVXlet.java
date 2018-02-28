cd16e91af058a288a2946364a3de39bcbe0c08b7
killerniky <niky.dieben@hotmail.com>
2018-02-28 14:52:31 +0100

    test

---
+++ HelloTVXlet_Test/src/hellotvxlet/HelloTVXlet.java
@@ -0,0 +1,29 @@
+package hellotvxlet;
+
+import javax.tv.xlet.*;
+
+
+public class HelloTVXlet implements Xlet {
+
+  
+    public HelloTVXlet() {
+        
+    }
+
+    public void initXlet(XletContext context) {
+      
+     
+    }
+
+    public void startXlet() {
+    
+    }
+
+    public void pauseXlet() {
+     
+    }
+
+    public void destroyXlet(boolean unconditional) {
+     
+    }
+}

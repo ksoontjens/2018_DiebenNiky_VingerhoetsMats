341ad5b0d91a6cc710730c02450ff49863afc540
theMats5 <mats.vingerhoets5@gmail.com>
2018-05-09 12:43:59 +0200

    buttons + eventhandler

--- HelloTVXlet_Test/src/hellotvxlet/HelloTVXlet.java
+++ HelloTVXlet_Test/src/hellotvxlet/HelloTVXlet.java
@@ -1,116 +1,157 @@
 package hellotvxlet;
 
-import java.awt.Color;
-import javax.tv.xlet.Xlet;
-import javax.tv.xlet.XletContext;
-import javax.tv.xlet.XletStateChangeException;
-import org.havi.ui.HScene;
-import org.havi.ui.HSceneFactory;
-import org.havi.ui.HStaticText;
-import org.havi.ui.HTextButton;
-import org.havi.ui.HVisible;
+import javax.tv.xlet.*;
+import org.dvb.ui.*;
+import java.awt.*;
 
-public class HelloTVXlet implements Xlet {
+import org.havi.ui.*;
 
+import java.awt.event.*;
+import org.havi.ui.event.*;
+
+public class HelloTVXlet implements Xlet, HActionListener {
+
+    private XletContext actueleXletContext;
+    private HScene scene;
+    private boolean debug=true;
     
-    public void initXlet(XletContext ctx) throws XletStateChangeException {
-        HScene scene=HSceneFactory.getInstance().getDefaultHScene();
+    private HTextButton knop1 , knop2, knop3,knop4,knop5,knop6,knop7,knop8,knop9;
+    
+   
+ 
+    public void actionPerformed (ActionEvent e){
+        System.out.println(e.getActionCommand());
+    }
+    public void initXlet(XletContext context) throws XletStateChangeException {
+        if(debug) System.out.println("Xlet Initialiseren");
+        this.actueleXletContext = context;
+        
+        HSceneTemplate sceneTemplate= new HSceneTemplate();
         // SCHERM = 720 x 576
         
-        //HStaticText hst=new HStaticText("Wie was de eerste president van de VS?",20,300,680,100); // tekst,x,y,w,h
-        //hst.setBackgroundMode(HVisible.BACKGROUND_FILL);
-       // hst.setBackground(Color.BLUE);
-        //scene.add(hst);
+        sceneTemplate.setPreference(HSceneTemplate.SCENE_SCREEN_DIMENSION, new HScreenDimension (1.0f, 1.0f), HSceneTemplate.REQUIRED );
+        sceneTemplate.setPreference(HSceneTemplate.SCENE_SCREEN_LOCATION, new HScreenPoint (0.0f, 0.0f), HSceneTemplate.REQUIRED );
         
+        scene = HSceneFactory.getInstance().getBestScene(sceneTemplate);
+            //kollom1
+            knop1 = new HTextButton("KNOP1");
+            knop1.setLocation(20,50);
+            knop1.setSize(150,150);
+            knop1.setBackground(Color.GRAY);
+            knop1.setBackgroundMode(HVisible.BACKGROUND_FILL);      
         
-        //first row
-        HTextButton button1=new HTextButton("",20,50,150,150); // tekst,x,y,w,h
-        button1.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        button1.setBackground(Color.GRAY);
-        scene.add(button1);
+            knop2 = new HTextButton("KNOP2");
+            knop2.setLocation(20,200);
+            knop2.setSize(150,150);
+            knop2.setBackground(Color.GRAY);
+            knop2.setBackgroundMode(HVisible.BACKGROUND_FILL);
+            
+            knop3 = new HTextButton("KNOP3");
+            knop3.setLocation(20,350);
+            knop3.setSize(150,150);
+            knop3.setBackground(Color.GRAY);
+            knop3.setBackgroundMode(HVisible.BACKGROUND_FILL);
+            
+           //kollom2
+            knop4 = new HTextButton("KNOP4");
+            knop4.setLocation(170,50);
+            knop4.setSize(150,150);
+            knop4.setBackground(Color.GRAY);
+            knop4.setBackgroundMode(HVisible.BACKGROUND_FILL);
+            
+            knop5 = new HTextButton("KNOP5");
+            knop5.setLocation(170,200);
+            knop5.setSize(150,150);
+            knop5.setBackground(Color.GRAY);
+            knop5.setBackgroundMode(HVisible.BACKGROUND_FILL);
+            
+            knop6 = new HTextButton("KNOP6");
+            knop6.setLocation(170,350);
+            knop6.setSize(150,150);
+            knop6.setBackground(Color.GRAY);
+            knop6.setBackgroundMode(HVisible.BACKGROUND_FILL);
+            
+            //kollom3
+            
+            knop7= new HTextButton("KNOP7");
+            knop7.setLocation(320,50);
+            knop7.setSize(150,150);
+            knop7.setBackground(Color.GRAY);
+            knop7.setBackgroundMode(HVisible.BACKGROUND_FILL);
+            
+            knop8 = new HTextButton("KNOP8");
+            knop8.setLocation(320,200);
+            knop8.setSize(150,150);
+            knop8.setBackground(Color.GRAY);
+            knop8.setBackgroundMode(HVisible.BACKGROUND_FILL);
+            
+            knop9 = new HTextButton("KNOP9");
+            knop9.setLocation(320,350);
+            knop9.setSize(150,150);
+            knop9.setBackground(Color.GRAY);
+            knop9.setBackgroundMode(HVisible.BACKGROUND_FILL);
+            
+            
+            
+       knop1. setFocusTraversal ( null , knop2, null , knop4 ) ; // op , neer , links ,rechts
+        knop2. setFocusTraversal (knop1, knop3 , null , knop5 ) ; // op , neer , links ,rechts
         
-        HTextButton button2=new HTextButton("",20,200,150,150); // tekst,x,y,w,h
-        button2.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        button2.setBackground(Color.GRAY);
-        scene.add(button2);
- 
-        HTextButton button3=new HTextButton("",20,350,150,150); // tekst,x,y,w,h
-        button3.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        button3.setBackground(Color.GRAY);
-        scene.add(button3);
-        
-        //2nd row
-        
-        HTextButton button4=new HTextButton("",170,50,150,150); // tekst,x,y,w,h
-        button4.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        button4.setBackground(Color.GRAY);
-        scene.add(button4);
-        
-        HTextButton button5=new HTextButton("",170,200,150,150); // tekst,x,y,w,h
-        button5.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        button5.setBackground(Color.GRAY);
-        scene.add(button5);
-        
-        HTextButton button6=new HTextButton("",170,350,150,150); // tekst,x,y,w,h
-        button6.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        button6.setBackground(Color.GRAY);
-        scene.add(button6);
-        
-        
-        //3rd row
-        HTextButton button7=new HTextButton("",320,50,150,150); // tekst,x,y,w,h
-        button7.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        button7.setBackground(Color.GRAY);
-        scene.add(button7);
-        
-        HTextButton button8=new HTextButton("",320,200,150,150); // tekst,x,y,w,h
-        button8.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        button8.setBackground(Color.GRAY);
-        scene.add(button8);
-        
-        HTextButton button9=new HTextButton("",320,350,150,150); // tekst,x,y,w,h
-        button9.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        button9.setBackground(Color.GRAY);
-        scene.add(button9);
-        
-        
-        button1. setFocusTraversal ( null , button2, null , button4 ) ; // op , neer , links ,rechts
-        button2. setFocusTraversal (button1, button3 , null , button5 ) ; // op , neer , links ,rechts
-        
-        button3. setFocusTraversal ( button2 , null, null , button6 ) ; // op , neer , links ,rechts
-        button4. setFocusTraversal ( null , button5, button1 , button7 ) ; // op , neer , links ,rechts
-        button5. setFocusTraversal ( button4 , button6, button2 , button8 ) ; // op , neer , links ,rechts
-        button6. setFocusTraversal ( button5 , null, button3 , button9 ) ; // op , neer , links ,rechts
-        button7. setFocusTraversal ( null , button8, button4 , null ) ; // op , neer , links ,rechts
-        button8. setFocusTraversal ( button7 , button9, button5 , null ) ; // op , neer , links ,rechts
-        button9. setFocusTraversal ( button8 , null, button6 , null ) ; // op , neer , links ,rechts
-        
-        
-        button1. requestFocus ( ) ;
+        knop3. setFocusTraversal ( knop2 , null, null , knop6 ) ; // op , neer , links ,rechts
+        knop4. setFocusTraversal ( null , knop5, knop1 , knop7 ) ; // op , neer , links ,rechts
+        knop5. setFocusTraversal ( knop4 , knop6, knop2 , knop8 ) ; // op , neer , links ,rechts
+        knop6. setFocusTraversal ( knop5 , null, knop3 , knop9 ) ; // op , neer , links ,rechts
+        knop7. setFocusTraversal ( null , knop8, knop4 , null ) ; // op , neer , links ,rechts
+        knop8. setFocusTraversal ( knop7 , knop9, knop5 , null ) ; // op , neer , links ,rechts
+        knop9. setFocusTraversal ( knop8 , null, knop6 , null ) ; // op , neer , links ,rechts
+    
+      knop1.setActionCommand("knop1_actioned");
+      knop2.setActionCommand("knop2_actioned");
+      knop3.setActionCommand("knop3_actioned");
+      knop4.setActionCommand("knop4_actioned");
+      knop5.setActionCommand("knop5_actioned");
+      knop6.setActionCommand("knop6_actioned");
+      knop7.setActionCommand("knop7_actioned");
+      knop8.setActionCommand("knop8_actioned");
+      knop9.setActionCommand("knop9_actioned");
+      
+      
+      knop1.addHActionListener(this);
+      knop2.addHActionListener(this);
+      knop3.addHActionListener(this);
+      knop4.addHActionListener(this);
+      knop5.addHActionListener(this);
+      knop6.addHActionListener(this);
+      knop7.addHActionListener(this);
+      knop8.addHActionListener(this);
+      knop9.addHActionListener(this);
+      
+      
+      scene.add(knop1);
+      scene.add(knop2);
+      scene.add(knop3);
+      scene.add(knop4);
+      scene.add(knop5);
+      scene.add(knop6);
+      scene.add(knop7);
+      scene.add(knop8);
+      scene.add(knop9);
+      
+      
+      knop1.requestFocus();
+    }    
+    
+    public void startXlet() throws XletStateChangeException {
+        if(debug) System.out.println("Xlet starten");
         
         scene.validate();
         scene.setVisible(true);
     }
-    public void startXlet() throws XletStateChangeException {
-     
-    }
+    
     public void pauseXlet() {
        
-    }
-
-    
-    public void destroyXlet(boolean unconditional) throws XletStateChangeException {
+    }    
+     public void destroyXlet(boolean unconditional) throws XletStateChangeException {
      
-    }
-    private String whoseTurn = "X";
-    private void determineWhoseTurn() {
-       if(whoseTurn.equalsIgnoreCase("X")){
-           whoseTurn = "O";
-           
-       }else{
-           whoseTurn = "X";
-       }
     }
 
   
-}
